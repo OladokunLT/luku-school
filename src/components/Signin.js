159b1f1a8176a481c64ffbbfd67188051b1ed632
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-05-24 22:52:05 +0100

    Removed the routing to Home page and restructured for styling

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -4,13 +4,17 @@ import { Link } from 'react-router-dom'
 function Signin() {
   return (
     <div>
-      <div>
-        <h1>Sign in to your account</h1>
-        <p>
-          Don't have an account yet? <Link to="/signup">Sign up</Link>
-        </p>
-      </div>
-      <form>
+      <section className='left-side'>
+        <div>
+          <h1>Student Login</h1>
+          <p>Make sure your account is secure</p>
+        </div>
+        <div className=''>
+          <img src="" alt="icon of student with laptop" className=''/>
+        </div>
+      </section>
+      
+      <form className='right-side'>
         <div className="">
           <label htmlFor="" >Email Address </label>
           <input type="email" /> 
@@ -22,6 +26,9 @@ function Signin() {
         <div>
           <button>Sign in</button>
         </div>
+        <p>
+          Don't have an account yet? <Link to="/signup"><button>Sign up</button></Link>
+        </p>
       </form>
     </div>
   )
