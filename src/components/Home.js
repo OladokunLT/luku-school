1ab92fefef79de16d715a0fccbba74d854b1b248
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-05-24 22:53:17 +0100

    Updated Home page and ready of styling.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,21 @@ import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <header>
+        <div className="logo">LOGO <span>LukuSchool</span></div>
+        <nav className='top-right-login-btn'>          
+          <Link to="/signin">
+            <button>Sign in</button>
+          </Link>
+        </nav>
+        <hr />
+      </header>
+      <main>
+        <h1>Welcome to LukuSchool</h1>
+        <h3> learning at its peak</h3>
+      </main>
+    </div>
   )
 }
 
