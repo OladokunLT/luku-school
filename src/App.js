3890b77965d8e84dc1625dd7d1a062d2efae029c
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-05-25 09:01:29 +0100

    Nested route (Courses and Profile) added for the dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import Courses from "./components/Courses";
+import Profile from "./components/Profile";
+// import Account from "./components/Account";
 
 function App() {
-  return (
+  return (  
     <div className="App">
-      <h1>Firebase authentication</h1>
+      {/* <h1>Firebase authentication</h1> */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/dashboard' element={<Dashboard />}> 
+          <Route path='/dashboard/courses' element={<Courses />} />
+          <Route path='/dashboard/profile' element={<Profile />} />
+        </Route>
+        {/* <Route path='/account' element={<Account />} /> */}
+      </Routes>
+      
     </div>
   );
 }
