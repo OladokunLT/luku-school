b4840f9e222f4227225b1f6282aa24001c06a067
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-05-26 22:28:07 +0100

    AuthContextProvider is used to wrap the existing Routes from line 17 to 28

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ import Signup from "./components/Signup";
 import Signin from "./components/Signin";
 import Courses from "./components/Courses";
 import Profile from "./components/Profile";
+import { AuthContextProvider } from './context/AuthContext';
 // import Account from "./components/Account";
 
 function App() {
   return (  
     <div className="App">
       {/* <h1>Firebase authentication</h1> */}
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/signin' element={<Signin />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/dashboard' element={<Dashboard />}> 
-          <Route path='/dashboard/courses' element={<Courses />} />
-          <Route path='/dashboard/profile' element={<Profile />} />
-        </Route>
-        {/* <Route path='/account' element={<Account />} /> */}
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/dashboard' element={<Dashboard />}> 
+            <Route path='/dashboard/courses' element={<Courses />} />
+            <Route path='/dashboard/profile' element={<Profile />} />
+          </Route>
+          {/* <Route path='/account' element={<Account />} /> */}
+        </Routes>
+      </AuthContextProvider>
       
     </div>
   );
