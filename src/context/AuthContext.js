11fa30004b89ac7b679db137d586089ab8fd36aa
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-05-29 15:37:54 +0100

    Updated all required children (user, signin, signout, and createUser) in AuthContextProvider

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { 
     createUserWithEmailAndPassword, 
     signInWithEmailAndPassword,
@@ -10,12 +10,32 @@ import { auth } from "../firebase";
 const UserContext = createContext()
 
 export const AuthContextProvider = ({children}) => {
+    const[user, setUser] = useState({});
+
     const createUser = (email, password) => {
-        return createUserWithEmailAndPassword(auth, email, password)
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
     }
 
+    const signout = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            console.log(currentUser);
+            setUser(currentUser);
+        });
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
     return (
-        <UserContext.Provider value={{createUser}}>
+        <UserContext.Provider value={{user, createUser, signIn, signout }}>
             {children}
         </UserContext.Provider> 
     )
