8bd04123ab43e41f6cfba47122dbcc2946aa5d05
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-05-29 16:19:16 +0100

    Modified

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+      <main>Profile</main>
+      <h3>Email: {}</h3>
+      <div>name: {}</div>
+      <img src="" alt="Profile image" />
+    </div>
+  )
+}
+
+export default Profile
