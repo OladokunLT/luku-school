1bab56fb0ef311b28de22655019e7019d11487d2
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-05-29 14:47:37 +0100

    ProtectedRoute added used to shroud element- Dashboad.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Signin from "./components/Signin";
 import Courses from "./components/Courses";
 import Profile from "./components/Profile";
 import { AuthContextProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 // import Account from "./components/Account";
 
 function App() {
@@ -19,14 +20,23 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/signin' element={<Signin />} />
           <Route path='/signup' element={<Signup />} />
-          <Route path='/dashboard' element={<Dashboard />}> 
+          <Route 
+            path='/dashboard' 
+            element= {
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          > 
             <Route path='/dashboard/courses' element={<Courses />} />
             <Route path='/dashboard/profile' element={<Profile />} />
           </Route>
           {/* <Route path='/account' element={<Account />} /> */}
         </Routes>
       </AuthContextProvider>
-      
+      <ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>
     </div>
   );
 }
