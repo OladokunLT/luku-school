6b9d59376125f3c3d012673defe4906501a4d413
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-05-30 02:01:22 +0100

    Displayed user information grabbed from Auth table.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { UserAuth } from '../context/AuthContext';
 
 function Profile() {
+  const {user} = UserAuth()
   return (
     <div>
       <main>Profile</main>
-      <h3>Email: {}</h3>
-      <div>name: {}</div>
-      <img src="" alt="Profile image" />
+      <h3>Email: {user.email}</h3>
+      <div>name: {user.displayName}</div>
+      <img src={user.photoURL} alt="image" />
     </div>
   )
 }
