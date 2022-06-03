0f7051dceffb9d126cecf72a2b35294c820e3592
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-06-03 03:08:42 +0100

    google signin with popup is modify to Redirect

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,54 +1,54 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  GoogleAuthProvider,
-  signInWithPopup,
+import { 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    GoogleAuthProvider,
+    // signInWithPopup,
+    signInWithRedirect
 } from "firebase/auth";
 import { auth } from "../firebase";
-const UserContext = createContext();
+const UserContext = createContext()
 
-export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState({});
+export const AuthContextProvider = ({children}) => {
+    const[user, setUser] = useState({});
 
-  const createUser = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
-  };
-
-  const signIn = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
-  };
-
-  const signout = () => {
-    return signOut(auth);
-  };
-
-  function googleSignIn() {
-    const googleAuthProvider = new GoogleAuthProvider();
-    return signInWithPopup(auth, googleAuthProvider);
-  }
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      console.log(currentUser);
-      setUser(currentUser);
-    });
-    return () => {
-      unsubscribe();
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
     };
-  }, []);
 
-  return (
-    <UserContext.Provider
-      value={{ user, createUser, signIn, signout, googleSignIn }}
-    >
-      {children}
-    </UserContext.Provider>
-  );
-};
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const signout = () => {
+        return signOut(auth)
+    }
+
+    function googleSignIn() {
+        const googleAuthProvider = new GoogleAuthProvider();
+        return signInWithRedirect(auth, googleAuthProvider)
+        // return signInWithPopup(auth, googleAuthProvider);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            console.log(currentUser);
+            setUser(currentUser);
+        });
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+    return (
+        <UserContext.Provider value={{user, createUser, signIn, signout, googleSignIn }}>
+            {children}
+        </UserContext.Provider> 
+    )
+}
 
 export const UserAuth = () => {
-  return useContext(UserContext);
-};
+    return useContext(UserContext)
+}
